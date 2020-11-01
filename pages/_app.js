c35592151bc2ee1f152c192c39ad3229cd067f61
Gayath1 <chandulagayan@gmail.com>
2020-11-01 15:35:26 +0530

    Simple Crud function

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,10 @@
-import '../styles/globals.css'
+import './Home.module.css'
+
+
 
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+    return <Component {...pageProps} />
 }
+
 
 export default MyApp
--- pages/index.js
+++ pages/index.js
@@ -1,65 +1,103 @@
-import Head from 'next/head'
-import styles from '../styles/Home.module.css'
+import React, { Component } from 'react';
+import './Home.module.css'
 
-export default function Home() {
-  return (
-    <div className={styles.container}>
-      <Head>
-        <title>Create Next App</title>
-        <link rel="icon" href="/favicon.ico" />
-      </Head>
 
-      <main className={styles.main}>
-        <h1 className={styles.title}>
-          Welcome to <a href="https://nextjs.org">Next.js!</a>
-        </h1>
 
-        <p className={styles.description}>
-          Get started by editing{' '}
-          <code className={styles.code}>pages/index.js</code>
-        </p>
 
-        <div className={styles.grid}>
-          <a href="https://nextjs.org/docs" className={styles.card}>
-            <h3>Documentation &rarr;</h3>
-            <p>Find in-depth information about Next.js features and API.</p>
-          </a>
 
-          <a href="https://nextjs.org/learn" className={styles.card}>
-            <h3>Learn &rarr;</h3>
-            <p>Learn about Next.js in an interactive course with quizzes!</p>
-          </a>
 
-          <a
-            href="https://github.com/vercel/next.js/tree/master/examples"
-            className={styles.card}
-          >
-            <h3>Examples &rarr;</h3>
-            <p>Discover and deploy boilerplate example Next.js projects.</p>
-          </a>
 
-          <a
-            href="https://vercel.com/import?filter=next.js&utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-            className={styles.card}
-          >
-            <h3>Deploy &rarr;</h3>
-            <p>
-              Instantly deploy your Next.js site to a public URL with Vercel.
-            </p>
-          </a>
-        </div>
-      </main>
+class App extends Component{
 
-      <footer className={styles.footer}>
-        <a
-          href="https://vercel.com?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Powered by{' '}
-          <img src="/vercel.svg" alt="Vercel Logo" className={styles.logo} />
-        </a>
-      </footer>
-    </div>
-  )
+
+    constructor(props)
+    {
+        super(props);
+        this.state = {
+            title: 'Admin Crud',
+            act:0,
+            index:'',
+            datas: []
+        }
+    }
+    componentDidMount(){
+        this.refs.name.focus();
+    }
+    fSubmit = (e) =>{
+        e.preventDefault();
+        console.log('try');
+
+        let datas = this.state.datas;
+        let name = this.refs.name.value;
+        let price = this.refs.price.value;
+        if(this.state.act === 0){   //new
+            let data = {
+                name, price
+            }
+            datas.push(data);
+        }else{                      //update
+            let index = this.state.index;
+            datas[index].name = name;
+            datas[index].price = price;
+        }
+        this.setState({
+            datas: datas,
+            act: 0
+        });
+        this.refs.myForm.reset();
+        this.refs.name.focus();
+    }
+    fRemove = (i) => {
+        let datas = this.state.datas;
+        datas.splice(i,1);
+        this.setState({
+            datas: datas
+        });
+
+        this.refs.myForm.reset();
+        this.refs.name.focus();
+    }
+    fEdit = (i) => {
+        let data = this.state.datas[i];
+        this.refs.name.value = data.name;
+        this.refs.price.value = data.price;
+
+        this.setState({
+            act: 1,
+            index: i
+        });
+
+        this.refs.name.focus();
+    }
+
+
+    render()
+    {
+        let datas = this.state.datas;
+        return (
+            <div className="app">
+                <h2>{this.state.title}</h2>
+                <form ref="myForm" className="myForm">
+                    <input type="text" ref="name" placeholder="Product name" className="formField" />
+                    <input type="text" ref="price" placeholder="Price" className="formField" />
+                    <button onClick={(e)=>this.fSubmit(e)} className="myButton">submit </button>
+                </form>
+                <pre>
+          {datas.map((data, i) =>
+              <li key={i} className="myList">
+                  {i+1}. {data.name}, {data.price}
+                  <button onClick={()=>this.fRemove(i)} className="myListButton">remove </button>
+                  <button onClick={()=>this.fEdit(i)} className="myListButton">edit </button>
+              </li>
+          )}
+        </pre>
+
+            </div>
+        );
+
+    }
 }
+
+
+
+export default App;
